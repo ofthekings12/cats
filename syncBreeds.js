7707e9367796449900a616bee5f1aa33ecbf44b4
Mikko De Los Reyes <mikkode212@gmail.com>
2021-12-01 20:38:33 +0000

    cats

---
+++ syncBreeds.js
@@ -0,0 +1,14 @@
+// data in memory
+const catBreeds = {
+  
+};
+
+// synchronous function to fetch a cat breed
+const breedDetails = function(breed) {
+  // can simply return it (easy peezee, butter squeezy) ...
+  return catBreeds[breed];
+};
+
+// get the return value right away from the function
+const bombay = breedDetails('Bombay');
+console.log(bombay); //=> prints out the description for that breed
